e0738d6486087943cfb9e61da6fd9fe78a70b9dc
htirawi <hmtirawi@gmail.com>
2018-09-13 17:49:35 +0300

    edit design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Article} from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://www.angular.io', 3),
+      new Article('React', 'https://reactjs.org/', 2),
+      new Article('Vue', 'https://vuejs.org/', 5),
+
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article with title ${title.value} and link ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = null;
+    link.value = null;
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
